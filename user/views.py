836b689a36e2ac6d4dec20c43fac11b50c6698ce
steven <ssss91809@gmail.com>
2024-03-14 21:49:09 +0800

    完成專案設定

---
+++ user/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+
+def user_register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        username = request.POST.get("username")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+        print(username, password1, password2)
+    return render(request, "user/register.html", {"form": form})
