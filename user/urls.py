16315c81ca0be84c0f82ab43fe2a467a15c37f87
steven <ssss91809@gmail.com>
2024-03-19 20:00:10 +0800

    完成登入功能

--- user/urls.py
+++ user/urls.py
@@ -21,5 +21,6 @@ from . import views
 
 
 urlpatterns = [
-    path("register/", views.user_register),
+    path("login/", views.user_login, name="login"),
+    path("register/", views.user_register, name="register"),
 ]
