4fcc5d29015d7d5d0bfc907bbeb030684334de37
steven <ssss91809@gmail.com>
2024-03-21 19:08:46 +0800

    初始化完成todo

--- todo/urls.py
+++ todo/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-from app import views
+from django.urls import path
+from . import views
 
-
+# 分配user次要路徑
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    # 分配user主要進入點
-    path("user/", include("user.urls")),
-    path("", include("todo.urls")),
+    path("", views.todo, name="logout"),
 ]
