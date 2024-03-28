87a8f4a70032a28fc672c6657422003c0922a83a
steven <ssss91809@gmail.com>
2024-03-28 20:32:38 +0800

    安裝mysql

--- todolist/settings.py
+++ todolist/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,6 +85,16 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "todolist",
+        "USER": "root",
+        "PASSWORD": "123456789",
+        "HOST": "127.0.0.1",
+        "POST": "3306",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
